40076d4d2abe84a59164eb0ddf33730fc0d5899b
tenten0113 <glemrinnogizumo@yahoo.co.jp>
2020-06-28 15:14:14 +0900

    rirst-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Counter from './components/Counter';
+import CounterHook from './components/CounterHook';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <CounterHook  /> */}
+      
     </div>
   );
 }
 
 export default App;
+
+/*
+・ユーザー名は半角英数（大文字はＯＫ）などで決定する
+（例：Chibiharu02←ＯＫ！,'あいうえおさん'←ＮＧ
+・パスワードは半角英数で[1-9,a-z,A-Z]で入力してもらう、6文字以上16字以内で
+・メールアドレスを＠で区切られているかどうかで区別する
+・両方の入力が正しくされていれば登録が出来る
+
+
+
+・文字を入力した時点でバリデーションが出る
+・保存ボタン押下時にバリデーション
+・保存ボタン押下時にモーダルが出て、保存、キャンセルボタンがある。押したらモーダルが閉じて、保存か or エラー。保存したら保存されましたのトースト or 画面になる
+ */
